fab253f5cff4c12f84cb1ac89a959226adfedfeb
nico <90756549+sengokus@users.noreply.github.com>
2024-05-29 16:05:22 +0800

    Changed tab bar to carousel

--- lib/components/agent_tabbar.dart
+++ lib/components/agent_tabbar.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:http/http.dart' as http;
 
 class AgentTab extends StatefulWidget {
@@ -21,8 +22,10 @@ class AgentTabState extends State<AgentTab> with AutomaticKeepAliveClientMixin {
     super.build(
         context); // Invoke the overridden method from AutomaticKeepAliveClientMixin
 
-    return FutureBuilder(
-      future: fetchAgents(),
+    final agents = fetchAgents();
+
+    return FutureBuilder<List<dynamic>>(
+      future: agents,
       builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator());
@@ -30,39 +33,83 @@ class AgentTabState extends State<AgentTab> with AutomaticKeepAliveClientMixin {
           if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else {
-            return SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  for (final agent in snapshot.data!)
-                    InkWell(
-                      onTap: () {
-                        widget.onAgentSelected(agent);
-                      },
-                      child: Container(
-                        width: 60,
-                        margin: const EdgeInsets.symmetric(horizontal: 4.0),
-                        decoration: BoxDecoration(
-                          color: widget.color,
-                          border: Border.all(
-                            color: Colors.white30,
-                            width: 1,
-                          ),
-                        ),
-                        child: Image.network(
-                          agent['displayIcon'],
-                          fit: BoxFit.cover,
+            return CarouselSlider(
+              options: CarouselOptions(
+                height: 60,
+                aspectRatio: 1.0,
+                viewportFraction: 0.2,
+                enableInfiniteScroll: true,
+              ),
+              items: snapshot.data!.map<Widget>((i) {
+                return InkWell(
+                    onTap: () {
+                      widget.onAgentSelected(i);
+                    },
+                    child: Container(
+                      width: 60,
+                      margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                      decoration: BoxDecoration(
+                        color: widget.color,
+                        border: Border.all(
+                          color: Colors.white30,
+                          width: 1,
                         ),
                       ),
-                    ),
-                ],
-              ),
+                      child: Image.network(
+                        i['displayIcon'],
+                        fit: BoxFit.cover,
+                      ),
+                    ));
+              }).toList(),
             );
           }
         }
       },
     );
   }
+
+  //   return FutureBuilder(
+  //     future: fetchAgents(),
+  //     builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+  //       if (snapshot.connectionState == ConnectionState.waiting) {
+  //         return const Center(child: CircularProgressIndicator());
+  //       } else {
+  //         if (snapshot.hasError) {
+  //           return Center(child: Text('Error: ${snapshot.error}'));
+  //         } else {
+  //           return SingleChildScrollView(
+  //             scrollDirection: Axis.horizontal,
+  //             child: Row(
+  //               children: [
+  //                 for (final agent in snapshot.data!)
+  //                   InkWell(
+  //                     onTap: () {
+  //                       widget.onAgentSelected(agent);
+  //                     },
+  //                     child: Container(
+  //                       width: 60,
+  //                       margin: const EdgeInsets.symmetric(horizontal: 4.0),
+  //                       decoration: BoxDecoration(
+  //                         color: widget.color,
+  //                         border: Border.all(
+  //                           color: Colors.white30,
+  //                           width: 1,
+  //                         ),
+  //                       ),
+  //                       child: Image.network(
+  //                         agent['displayIcon'],
+  //                         fit: BoxFit.cover,
+  //                       ),
+  //                     ),
+  //                   ),
+  //               ],
+  //             ),
+  //           );
+  //         }
+  //       }
+  //     },
+  //   );
+  // }
 
   Future<List<dynamic>> fetchAgents() async {
     final response =
