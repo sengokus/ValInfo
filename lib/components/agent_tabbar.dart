bde00d4021048a0af3ef27f176d4b08d21182ef9
nico <90756549+sengokus@users.noreply.github.com>
2024-05-30 14:59:17 +0800

    Started PageView builder for main screen

--- lib/components/agent_tabbar.dart
+++ lib/components/agent_tabbar.dart
@@ -1,3 +1,130 @@
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:carousel_slider/carousel_slider.dart';
+// import 'package:http/http.dart' as http;
+
+// class AgentTab extends StatefulWidget {
+//   final Color? color;
+//   final Function(dynamic) onAgentSelected;
+
+//   const AgentTab({this.color, required this.onAgentSelected, super.key});
+
+//   @override
+//   AgentTabState createState() => AgentTabState();
+// }
+
+// class AgentTabState extends State<AgentTab> with AutomaticKeepAliveClientMixin {
+//   @override
+//   bool get wantKeepAlive => true;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     super.build(
+//         context); // Invoke the overridden method from AutomaticKeepAliveClientMixin
+
+//     final agents = fetchAgents();
+
+//     return FutureBuilder<List<dynamic>>(
+//       future: agents,
+//       builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+//         if (snapshot.connectionState == ConnectionState.waiting) {
+//           return const Center(child: CircularProgressIndicator());
+//         } else {
+//           if (snapshot.hasError) {
+//             return Center(child: Text('Error: ${snapshot.error}'));
+//           } else {
+//             return CarouselSlider(
+//               options: CarouselOptions(
+//                 height: 60,
+//                 aspectRatio: 1.0,
+//                 viewportFraction: 0.2,
+//                 enableInfiniteScroll: true,
+//               ),
+//               items: snapshot.data!.map<Widget>((i) {
+//                 return InkWell(
+//                     onTap: () {
+//                       widget.onAgentSelected(i);
+//                     },
+//                     child: Container(
+//                       width: 60,
+//                       margin: const EdgeInsets.symmetric(horizontal: 4.0),
+//                       decoration: BoxDecoration(
+//                         color: widget.color,
+//                         border: Border.all(
+//                           color: Colors.white30,
+//                           width: 1,
+//                         ),
+//                       ),
+//                       child: Image.network(
+//                         i['displayIcon'],
+//                         fit: BoxFit.cover,
+//                       ),
+//                     ));
+//               }).toList(),
+//             );
+//           }
+//         }
+//       },
+//     );
+//   }
+
+//   //   return FutureBuilder(
+//   //     future: fetchAgents(),
+//   //     builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+//   //       if (snapshot.connectionState == ConnectionState.waiting) {
+//   //         return const Center(child: CircularProgressIndicator());
+//   //       } else {
+//   //         if (snapshot.hasError) {
+//   //           return Center(child: Text('Error: ${snapshot.error}'));
+//   //         } else {
+//   //           return SingleChildScrollView(
+//   //             scrollDirection: Axis.horizontal,
+//   //             child: Row(
+//   //               children: [
+//   //                 for (final agent in snapshot.data!)
+//   //                   InkWell(
+//   //                     onTap: () {
+//   //                       widget.onAgentSelected(agent);
+//   //                     },
+//   //                     child: Container(
+//   //                       width: 60,
+//   //                       margin: const EdgeInsets.symmetric(horizontal: 4.0),
+//   //                       decoration: BoxDecoration(
+//   //                         color: widget.color,
+//   //                         border: Border.all(
+//   //                           color: Colors.white30,
+//   //                           width: 1,
+//   //                         ),
+//   //                       ),
+//   //                       child: Image.network(
+//   //                         agent['displayIcon'],
+//   //                         fit: BoxFit.cover,
+//   //                       ),
+//   //                     ),
+//   //                   ),
+//   //               ],
+//   //             ),
+//   //           );
+//   //         }
+//   //       }
+//   //     },
+//   //   );
+//   // }
+
+//   Future<List<dynamic>> fetchAgents() async {
+//     final response =
+//         await http.get(Uri.parse('https://valorant-api.com/v1/agents'));
+//     final data = jsonDecode(response.body);
+//     final List<dynamic> agents = data['data'];
+
+//     // Filter out the duplicate Sova from the API
+//     final playableAgents =
+//         agents.where((agent) => agent['isPlayableCharacter'] == true).toList();
+
+//     return playableAgents;
+//   }
+// }
+
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
@@ -5,7 +132,7 @@ import 'package:http/http.dart' as http;
 
 class AgentTab extends StatefulWidget {
   final Color? color;
-  final Function(dynamic) onAgentSelected;
+  final Function(Map<String, dynamic>) onAgentSelected;
 
   const AgentTab({this.color, required this.onAgentSelected, super.key});
 
@@ -19,8 +146,7 @@ class AgentTabState extends State<AgentTab> with AutomaticKeepAliveClientMixin {
 
   @override
   Widget build(BuildContext context) {
-    super.build(
-        context); // Invoke the overridden method from AutomaticKeepAliveClientMixin
+    super.build(context);
 
     final agents = fetchAgents();
 
@@ -40,10 +166,12 @@ class AgentTabState extends State<AgentTab> with AutomaticKeepAliveClientMixin {
                 viewportFraction: 0.2,
                 enableInfiniteScroll: true,
               ),
-              items: snapshot.data!.map<Widget>((i) {
+              items: snapshot.data!.asMap().entries.map<Widget>((entry) {
+                int index = entry.key;
+                var agent = entry.value;
                 return InkWell(
                     onTap: () {
-                      widget.onAgentSelected(i);
+                      widget.onAgentSelected({'index': index, 'agent': agent});
                     },
                     child: Container(
                       width: 60,
@@ -56,7 +184,7 @@ class AgentTabState extends State<AgentTab> with AutomaticKeepAliveClientMixin {
                         ),
                       ),
                       child: Image.network(
-                        i['displayIcon'],
+                        agent['displayIcon'],
                         fit: BoxFit.cover,
                       ),
                     ));
@@ -68,56 +196,12 @@ class AgentTabState extends State<AgentTab> with AutomaticKeepAliveClientMixin {
     );
   }
 
-  //   return FutureBuilder(
-  //     future: fetchAgents(),
-  //     builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
-  //       if (snapshot.connectionState == ConnectionState.waiting) {
-  //         return const Center(child: CircularProgressIndicator());
-  //       } else {
-  //         if (snapshot.hasError) {
-  //           return Center(child: Text('Error: ${snapshot.error}'));
-  //         } else {
-  //           return SingleChildScrollView(
-  //             scrollDirection: Axis.horizontal,
-  //             child: Row(
-  //               children: [
-  //                 for (final agent in snapshot.data!)
-  //                   InkWell(
-  //                     onTap: () {
-  //                       widget.onAgentSelected(agent);
-  //                     },
-  //                     child: Container(
-  //                       width: 60,
-  //                       margin: const EdgeInsets.symmetric(horizontal: 4.0),
-  //                       decoration: BoxDecoration(
-  //                         color: widget.color,
-  //                         border: Border.all(
-  //                           color: Colors.white30,
-  //                           width: 1,
-  //                         ),
-  //                       ),
-  //                       child: Image.network(
-  //                         agent['displayIcon'],
-  //                         fit: BoxFit.cover,
-  //                       ),
-  //                     ),
-  //                   ),
-  //               ],
-  //             ),
-  //           );
-  //         }
-  //       }
-  //     },
-  //   );
-  // }
-
   Future<List<dynamic>> fetchAgents() async {
     final response =
         await http.get(Uri.parse('https://valorant-api.com/v1/agents'));
     final data = jsonDecode(response.body);
     final List<dynamic> agents = data['data'];
 
-    // Filter out the duplicate Sova from the API
     final playableAgents =
         agents.where((agent) => agent['isPlayableCharacter'] == true).toList();
 
--- lib/pages/agent_info.dart
+++ lib/pages/agent_info.dart
@@ -1,3 +1,175 @@
+// import 'dart:developer';
+// import 'package:flutter/material.dart';
+// import 'package:valinfo/components/agent_info_button.dart';
+// import 'package:valinfo/components/agent_tabbar.dart';
+// import 'specific_agent_info.dart';
+
+// class AgentInfo extends StatefulWidget {
+//   final dynamic agent;
+
+//   const AgentInfo({
+//     required this.agent,
+//     super.key,
+//   });
+
+//   @override
+//   AgentInfoState createState() => AgentInfoState();
+// }
+
+// class AgentInfoState extends State<AgentInfo> {
+//   String? agentName;
+//   String? agentPhotoUrl;
+//   String? agentDescription;
+//   String? agentIcon;
+
+//   String? agentRoleName;
+//   String? agentRoleIcon;
+//   String? agentRoleDescription;
+
+//   bool isPressedFavorite = false;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     fetchAgentData(widget.agent);
+//   }
+
+//   // Function to fetch agent data
+//   void fetchAgentData(dynamic agent) {
+//     log("Fetching data for: ${agent['displayName']} : ${agent['role'] != null ? agent['role']['displayName'] : null}");
+
+//     setState(() {
+//       agentName = agent['displayName'];
+//       agentPhotoUrl = agent['fullPortrait'];
+//       agentDescription = agent['description'];
+//       agentRoleName =
+//           agent['role'] != null ? agent['role']['displayName'] : null;
+//       agentRoleIcon =
+//           agent['role'] != null ? agent['role']['displayIcon'] : null;
+//       agentRoleDescription =
+//           agent['role'] != null ? agent['role']['description'] : null;
+//     });
+//   }
+
+//   void updateSelectedAgent(dynamic agent) {
+//     setState(() {
+//       agentName = agent['displayName'];
+//       agentPhotoUrl = agent['fullPortrait'];
+//       agentDescription = agent['description'];
+//       agentRoleName =
+//           agent['role'] != null ? agent['role']['displayName'] : null;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: Container(
+//       decoration: const BoxDecoration(
+//           gradient: LinearGradient(
+//         colors: [Color(0xff3e606c), Color(0xff4c7a8a), Color(0xff222042)],
+//         stops: [0, 0.12, 1],
+//         begin: Alignment(-0.4, -1.0),
+//         end: Alignment(1.9, 2.3),
+//       )),
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Stack(
+//             clipBehavior: Clip.none,
+//             alignment: AlignmentDirectional.bottomEnd,
+//             children: [
+//               Positioned(
+//                 top: 20,
+//                 right: 20,
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.end,
+//                   children: [
+//                     Text(
+//                       agentName ?? 'Loading...',
+//                       style: TextStyle(
+//                         color: Theme.of(context).textTheme.titleMedium!.color,
+//                         fontFamily:
+//                             Theme.of(context).textTheme.titleMedium!.fontFamily,
+//                         fontSize:
+//                             Theme.of(context).textTheme.titleMedium!.fontSize,
+//                       ),
+//                     ),
+//                     Text(
+//                       agentRoleName ?? 'Loading...',
+//                       style: TextStyle(
+//                         fontFamily:
+//                             Theme.of(context).textTheme.titleSmall!.fontFamily,
+//                         fontSize:
+//                             Theme.of(context).textTheme.titleSmall!.fontSize,
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.all(8),
+//                 child: Image.network(agentPhotoUrl!,
+//                     fit: BoxFit.fitHeight,
+//                     height: 610, loadingBuilder: (BuildContext context,
+//                         Widget child, ImageChunkEvent? loadingProgress) {
+//                   if (loadingProgress == null) {
+//                     return child;
+//                   }
+//                   return const SizedBox(height: 500);
+//                 }),
+//               ),
+//             ],
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.all(20),
+//             child: Row(
+//               children: [
+//                 AgentInfoButton(
+//                   onPressed: () {
+//                     setState(() {
+//                       isPressedFavorite = !isPressedFavorite;
+//                     });
+//                   },
+//                   buttonText: isPressedFavorite ? "FAVORITED" : "FAVORITE",
+//                   backgroundColor: isPressedFavorite
+//                       ? Theme.of(context).indicatorColor
+//                       : Theme.of(context).hoverColor,
+//                 ),
+//                 const SizedBox(width: 5),
+//                 AgentInfoButton(
+//                   buttonText: "VIEW CONTRACT",
+//                   backgroundColor: Theme.of(context).hoverColor,
+//                   onPressed: () {
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(
+//                         builder: (context) => AgentDetailsPage(
+//                           agentName: agentName!,
+//                           agentPhotoUrl: agentPhotoUrl!,
+//                           agentDescription: agentDescription!,
+//                           agentRole: agentRoleName ?? 'Loading',
+//                           agentRoleIcon: agentRoleIcon ?? 'Loading',
+//                           agentRoleDescription:
+//                               agentRoleDescription ?? 'Loading',
+//                         ),
+//                       ),
+//                     );
+//                   },
+//                 ),
+//               ],
+//             ),
+//           ),
+//           AgentTab(
+//             color: Theme.of(context).indicatorColor,
+//             onAgentSelected: updateSelectedAgent,
+//           ),
+//         ],
+//       ),
+//     ));
+//   }
+// }
+
 import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:valinfo/components/agent_info_button.dart';
@@ -27,11 +199,20 @@ class AgentInfoState extends State<AgentInfo> {
   String? agentRoleDescription;
 
   bool isPressedFavorite = false;
+  late PageController _pageController;
+  int _currentPageIndex = 0;
 
   @override
   void initState() {
     super.initState();
+    _pageController = PageController();
     fetchAgentData(widget.agent);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   // Function to fetch agent data
@@ -51,121 +232,155 @@ class AgentInfoState extends State<AgentInfo> {
     });
   }
 
-  void updateSelectedAgent(dynamic agent) {
+  void updateSelectedAgent(Map<String, dynamic> selectedAgent) {
     setState(() {
-      agentName = agent['displayName'];
-      agentPhotoUrl = agent['fullPortrait'];
-      agentDescription = agent['description'];
-      agentRoleName =
-          agent['role'] != null ? agent['role']['displayName'] : null;
+      _currentPageIndex = selectedAgent['index'];
+      fetchAgentData(selectedAgent['agent']);
+      _pageController.jumpToPage(_currentPageIndex);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-        colors: [Color(0xff3e606c), Color(0xff4c7a8a), Color(0xff222042)],
-        stops: [0, 0.12, 1],
-        begin: Alignment(-0.4, -1.0),
-        end: Alignment(1.9, 2.3),
-      )),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Stack(
-            clipBehavior: Clip.none,
-            alignment: AlignmentDirectional.bottomEnd,
-            children: [
-              Positioned(
-                top: 20,
-                right: 20,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      agentName ?? 'Loading...',
-                      style: TextStyle(
-                        color: Theme.of(context).textTheme.titleMedium!.color,
-                        fontFamily:
-                            Theme.of(context).textTheme.titleMedium!.fontFamily,
-                        fontSize:
-                            Theme.of(context).textTheme.titleMedium!.fontSize,
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          colors: [Color(0xff3e606c), Color(0xff4c7a8a), Color(0xff222042)],
+          stops: [0, 0.12, 1],
+          begin: Alignment(-0.4, -1.0),
+          end: Alignment(1.9, 2.3),
+        )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: PageView.builder(
+                controller: _pageController,
+                itemCount: 24, // Assuming you have the agents list available
+                onPageChanged: (index) {
+                  setState(() {
+                    _currentPageIndex = index;
+                    List<dynamic> agents =
+                        []; // Add this line to define the 'agents' field
+
+                    fetchAgentData(agents[
+                        index]); // Update the code to use the 'agents' field
+                  });
+                },
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Stack(
+                        clipBehavior: Clip.none,
+                        alignment: AlignmentDirectional.bottomEnd,
+                        children: [
+                          Positioned(
+                            top: 20,
+                            right: 20,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  agentName ?? 'Loading...',
+                                  style: TextStyle(
+                                    color: Theme.of(context)
+                                        .textTheme
+                                        .titleMedium!
+                                        .color,
+                                    fontFamily: Theme.of(context)
+                                        .textTheme
+                                        .titleMedium!
+                                        .fontFamily,
+                                    fontSize: Theme.of(context)
+                                        .textTheme
+                                        .titleMedium!
+                                        .fontSize,
+                                  ),
+                                ),
+                                Text(
+                                  agentRoleName ?? 'Loading...',
+                                  style: TextStyle(
+                                    fontFamily: Theme.of(context)
+                                        .textTheme
+                                        .titleSmall!
+                                        .fontFamily,
+                                    fontSize: Theme.of(context)
+                                        .textTheme
+                                        .titleSmall!
+                                        .fontSize,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Image.network(agentPhotoUrl!,
+                                fit: BoxFit.fitHeight, height: 610,
+                                loadingBuilder: (BuildContext context,
+                                    Widget child,
+                                    ImageChunkEvent? loadingProgress) {
+                              if (loadingProgress == null) {
+                                return child;
+                              }
+                              return const SizedBox(height: 500);
+                            }),
+                          ),
+                        ],
                       ),
-                    ),
-                    Text(
-                      agentRoleName ?? 'Loading...',
-                      style: TextStyle(
-                        fontFamily:
-                            Theme.of(context).textTheme.titleSmall!.fontFamily,
-                        fontSize:
-                            Theme.of(context).textTheme.titleSmall!.fontSize,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: Image.network(agentPhotoUrl!,
-                    fit: BoxFit.fitHeight,
-                    height: 610, loadingBuilder: (BuildContext context,
-                        Widget child, ImageChunkEvent? loadingProgress) {
-                  if (loadingProgress == null) {
-                    return child;
-                  }
-                  return const SizedBox(height: 500);
-                }),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Row(
-              children: [
-                AgentInfoButton(
-                  onPressed: () {
-                    setState(() {
-                      isPressedFavorite = !isPressedFavorite;
-                    });
-                  },
-                  buttonText: isPressedFavorite ? "FAVORITED" : "FAVORITE",
-                  backgroundColor: isPressedFavorite
-                      ? Theme.of(context).indicatorColor
-                      : Theme.of(context).hoverColor,
-                ),
-                const SizedBox(width: 5),
-                AgentInfoButton(
-                  buttonText: "VIEW CONTRACT",
-                  backgroundColor: Theme.of(context).hoverColor,
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => AgentDetailsPage(
-                          agentName: agentName!,
-                          agentPhotoUrl: agentPhotoUrl!,
-                          agentDescription: agentDescription!,
-                          agentRole: agentRoleName ?? 'Loading',
-                          agentRoleIcon: agentRoleIcon ?? 'Loading',
-                          agentRoleDescription:
-                              agentRoleDescription ?? 'Loading',
+                      Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Row(
+                          children: [
+                            AgentInfoButton(
+                              onPressed: () {
+                                setState(() {
+                                  isPressedFavorite = !isPressedFavorite;
+                                });
+                              },
+                              buttonText:
+                                  isPressedFavorite ? "FAVORITED" : "FAVORITE",
+                              backgroundColor: isPressedFavorite
+                                  ? Theme.of(context).indicatorColor
+                                  : Theme.of(context).hoverColor,
+                            ),
+                            const SizedBox(width: 5),
+                            AgentInfoButton(
+                              buttonText: "VIEW CONTRACT",
+                              backgroundColor: Theme.of(context).hoverColor,
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => AgentDetailsPage(
+                                      agentName: agentName!,
+                                      agentPhotoUrl: agentPhotoUrl!,
+                                      agentDescription: agentDescription!,
+                                      agentRole: agentRoleName ?? 'Loading',
+                                      agentRoleIcon: agentRoleIcon ?? 'Loading',
+                                      agentRoleDescription:
+                                          agentRoleDescription ?? 'Loading',
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
                         ),
                       ),
-                    );
-                  },
-                ),
-              ],
+                    ],
+                  );
+                },
+              ),
             ),
-          ),
-          AgentTab(
-            color: Theme.of(context).indicatorColor,
-            onAgentSelected: updateSelectedAgent,
-          ),
-        ],
+            AgentTab(
+              color: Theme.of(context).indicatorColor,
+              onAgentSelected: updateSelectedAgent,
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
