7136ff57852dd0dce91a4f29fc287e6d67cc4f4d
zaxepaz <justinluistiu@gmail.com>
2024-05-31 12:05:18 +0800

    Changed UI for Appbar in agents_role.dart
    Tried to implement gesture dtector

--- lib/pages/agents_role.dart
+++ lib/pages/agents_role.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:valinfo/pages/specific_agent_info.dart';
 
 class AgentRolePage extends StatelessWidget {
   final List<dynamic> agents;
@@ -10,7 +11,7 @@ class AgentRolePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Agents with role: $role'),
+        title: Text('Agents'),
       ),
       body: Column(
         children: [
@@ -36,8 +37,38 @@ class AgentRolePage extends StatelessWidget {
                 var agent = agents[index];
                 String agentPhotoUrl = agent['displayIcon'];
 
-                return Card(
-                  child: Image.network(agentPhotoUrl, fit: BoxFit.cover),
+                return GestureDetector(
+                  onTap: () {
+                    // Navigate to AgentDetailsPage when the card is tapped
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AgentDetailsPage(
+                          agentName: agent['displayName'],
+                          agentPhotoUrl: agentPhotoUrl,
+                          agentDescription: agent['description'],
+                          agentRole: role,
+                          agentRoleIcon: 'Role Icon URL',
+                          agentRoleDescription: 'Role Description',
+                          agentAbility1Name: 'Ability 1 Name',
+                          agentAbility1Description: 'Ability 1 Description',
+                          agentAbility1Icon: 'Ability 1 Icon URL',
+                          agentAbility2Name: 'Ability 2 Name',
+                          agentAbility2Description: 'Ability 2 Description',
+                          agentAbility2Icon: 'Ability 2 Icon URL',
+                          agentAbility3Name: 'Ability 3 Name',
+                          agentAbility3Description: 'Ability 3 Description',
+                          agentAbility3Icon: 'Ability 3 Icon URL',
+                          agentAbility4Name: 'Ability 4 Name',
+                          agentAbility4Description: 'Ability 4 Description',
+                          agentAbility4Icon: 'Ability 4 Icon URL',
+                        ),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    child: Image.network(agentPhotoUrl, fit: BoxFit.cover),
+                  ),
                 );
               },
             ),
@@ -47,3 +78,4 @@ class AgentRolePage extends StatelessWidget {
     );
   }
 }
+
