a9d9ef6c1da2e581dcc1104cb3c97b9e1223bbc1
nico <90756549+sengokus@users.noreply.github.com>
2024-05-31 14:19:30 +0800

    Update

--- lib/components/agent_tabbar.dart
+++ lib/components/agent_tabbar.dart
@@ -4,8 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:http/http.dart' as http;
 
-
- class AgentTab extends StatefulWidget {
+class AgentTab extends StatefulWidget {
   final Color? color;
   final Function(Map<String, dynamic>) onAgentSelected;
   final int currentIndex; // Added currentIndex
@@ -57,52 +56,34 @@ class AgentTabState extends State<AgentTab> with AutomaticKeepAliveClientMixin {
                 color: Colors.white30,
                 width: 1,
               ),
-              items: snapshot.data!.asMap().entries.map<Widget>((entry) {
-                int index = entry.key;
-                var agent = entry.value;
-                return InkWell(
-                    onTap: () {
-                      widget.onAgentSelected({'index': index, 'agent': agent});
-                    },
-                    child: Container(
-                      width: 60,
-                      margin: const EdgeInsets.symmetric(horizontal: 4.0),
-                      decoration: BoxDecoration(
-                        color: widget.color,
-                        border: Border.all(
-                          color: Colors.white30,
-                          width: 1,
-                        ),
-                      ),
-                      child: Image.network(
-                        agent['displayIcon'],
-                        fit: BoxFit.cover,
-                      ),
-                    ));
-              }).toList(),
-            );
-          }
-        }
-      },
+            ),
+            child: Image.network(
+              agent['displayIcon'],
+              fit: BoxFit.cover,
+            ),
+          ),
+        );
+      }).toList(),
     );
-  }
-
-  Future<List<dynamic>> fetchAgents() async {
-    final response =
-        await http.get(Uri.parse('https://valorant-api.com/v1/agents'));
-    final data = jsonDecode(response.body);
-    final List<dynamic> agents = data['data'];
-
-    final playableAgents =
-        agents.where((agent) => agent['isPlayableCharacter'] == true).toList();
-
-    return playableAgents;
   }
 }
 
-//fetch agents based on their role
+Future<List<dynamic>> fetchAgents() async {
+  final response =
+      await http.get(Uri.parse('https://valorant-api.com/v1/agents'));
+  final data = jsonDecode(response.body);
+  final List<dynamic> agents = data['data'];
+
+  final playableAgents =
+      agents.where((agent) => agent['isPlayableCharacter'] == true).toList();
+
+  return playableAgents;
+}
+
+// Fetch agents based on their role
 Future<List<dynamic>> fetchAgentsRole({String? role}) async {
-  final response = await http.get(Uri.parse('https://valorant-api.com/v1/agents'));
+  final response =
+      await http.get(Uri.parse('https://valorant-api.com/v1/agents'));
   if (response.statusCode != 200) {
     throw Exception('Failed to fetch agents data');
   }
@@ -111,11 +92,15 @@ Future<List<dynamic>> fetchAgentsRole({String? role}) async {
   final List<dynamic> agents = data['data'];
 
   // Filter agents who are playable characters
-  final playableAgents = agents.where((agent) => agent['isPlayableCharacter'] == true).toList();
+  final playableAgents =
+      agents.where((agent) => agent['isPlayableCharacter'] == true).toList();
 
   // If role is specified, filter by role
   if (role != null && role.isNotEmpty) {
-    return playableAgents.where((agent) => agent['role'] != null && agent['role']['displayName'] == role).toList();
+    return playableAgents
+        .where((agent) =>
+            agent['role'] != null && agent['role']['displayName'] == role)
+        .toList();
   }
 
   return playableAgents;
